94d8a1366944beec27a69a70d2f1834c6677091e
collo <qollomcbizzy64@gmail.com>
2019-03-05 23:48:16 +0300

    editted the env key

--- config/app.php
+++ config/app.php
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('5I7Tc2LgexrJnNRqa5lc0WdW2actMNemPhxyN9pFffs'),
+    'key' => env('APP_KEY','5I7Tc2LgexrJnNRqa5lc0WdW2actMNemPhxyN9pFffs'),
 
     'cipher' => 'AES-256-CBC',
     'log'=>'errorlog',
